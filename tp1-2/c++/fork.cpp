7d19b889c085ccd24b308120ec0d8fee0ce86823
bob <bob@servicios.mshome.net>
2024-04-23 22:20:41 -0300

    add changes

--- tp1-2/c++/fork.cpp
+++ tp1-2/c++/fork.cpp
@@ -2,14 +2,15 @@
 #include <vector>
 #include <thread>
 #include <chrono>
-#include "fork.h"
+//#include "fork.h"
 
 const int N = 80;
 const int threadCount = 4;
 const int constant = 20;
 const bool useThreads = true;
 std::vector<std::vector<int>> matrix(N, std::vector<int>(N));
-std::vector<std::vector<int>> matrixRes(N, std::vector<int>(N));
+std::vector<std::vector<int>> matrixRS(N, std::vector<int>(N));
+std::vector<std::vector<int>> matrixRC(N, std::vector<int>(N));
 
 void updateMatrix()
 {
@@ -17,7 +18,7 @@ void updateMatrix()
     {
         for (int j = 0; j < N; ++j)
         {
-            matrixRes[i][j] = matrix[i][j] * constant;
+            matrixRS[i][j] = matrix[i][j] * constant;
         }
     }
 }
@@ -29,7 +30,7 @@ void udpateMatrixWithIndex(int initElement, int endElement)
         int column = i % N;
         if (row < N && column < N)
         {
-            matrixRes[row][column] = matrix[row][column] * constant;
+            matrixRC[row][column] = matrix[row][column] * constant;
         }
     }
 }
@@ -54,6 +55,23 @@ void updateMatrixByThreads()
         thread.join();
     }
 }
+
+bool checkResults()
+{
+    bool equal = true;
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            if(matrixRS[i][j] != matrixRC[i][j])
+            {
+                equal = false;
+            }
+        }
+    }
+    return equal;
+}
+
 void printMatrix(std::vector<std::vector<int>> matrix)
 {
     for (int i = 0; i < N; ++i)
@@ -76,25 +94,31 @@ int main()
             matrix[i][j] = rand() % 10;
         }
     }
-    // initial time
+
+
     auto start = std::chrono::high_resolution_clock::now();
-    if (useThreads)
-    {
-        updateMatrixByThreads();
-    }
-    else
-    {
-        updateMatrix();
-    }
-
-    // end time
+    updateMatrixByThreads();
     auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
+    std::chrono::duration<double> durationC = end - start;
 
-    std::cout << "initial matrix:\n";
-    printMatrix(matrix);
-    std::cout << "updated matrix:\n";
-    printMatrix(matrixRes);
-    std::cout << "total time in seconds: " << duration.count() << "\n";
+    start = std::chrono::high_resolution_clock::now();
+    updateMatrix();
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> durationS = end - start;
+
+    std::cout << "Matriz de " << N << " X " << N << "\n";
+    std::cout << "Cantidad de hilos: " << threadCount << "\n";
+
+    if(checkResults())
+        std::cout << "Las matrices RS y RC son iguales." << "\n";
+    else
+        std::cout << "Las matrices RS y RC son distintas." << "\n";
+
+    //std::cout << "initial matrix:\n";
+    //printMatrix(matrix);
+    //std::cout << "updated matrix:\n";
+    //printMatrix(matrixRes);
+    std::cout << "tiempo de ejecucion con calculo secuencial en milisegundos: " << durationS.count()*1000 << "\n";
+    std::cout << "tiempo de ejecucion con calculo concurrente en milisegundos: " << durationC.count()*1000 << "\n";
     return 0;
 }
