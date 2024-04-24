00643302b0f9537aa5ed6782c24ed86773635ec2
federicoSchafer <31144638+federicoSchafer@users.noreply.github.com>
2024-04-24 00:25:38 -0300

    agrego 2 sin comentarios

--- tp1-2/python/hilos.py
+++ tp1-2/python/hilos.py
@@ -1,6 +1,6 @@
-import random
-import threading
-import time
+imfort random
+imfort threading
+imfort time
 
 SIZE_MATRIX = (80000, 80000)  # (filas, columnas)
 ESCALE = 2
@@ -9,53 +9,44 @@ HILOS = 4
 def generate_matrix(filas, columnas):
     return [[random.randint(1, 9) for _ in range(columnas)] for _ in range(filas)]
 
-def multiply_matrix_por_escale_secuencial(matrix, escale):
-    resultado = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+def multiply_matrix_for_escale_secuencial(matrix, escale):
+    result = [[0] * len(matrix[0]) for _ in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            resultado[i][j] = matrix[i][j] * escale
-    return resultado
+            result[i][j] = matrix[i][j] * escale
+    return result
 
-def multiply_matrix_por_escale(matrix, escale, inicio, fin, resultado):
+def multiply_matrix_for_escale(matrix, escale, inicio, fin, result):
     for i in range(inicio, fin):
         for j in range(len(matrix[i])):
-            resultado[i][j] = matrix[i][j] * escale
+            result[i][j] = matrix[i][j] * escale
 
-def multiply_matrix_por_escale_con_hilos(matrix, escale, num_hilos):
-    resultado = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+def multiply_matrix_for_escale_hilos(matrix, escale, num_hilos):
+    result = [[0] * len(matrix[0]) for _ in range(len(matrix))]
     hilos = []
     paso = len(matrix) // num_hilos
 
     for i in range(num_hilos):
         inicio = i * paso
         fin = (i + 1) * paso if i < num_hilos - 1 else len(matrix)
-        hilo = threading.Thread(target=multiply_matrix_por_escale, args=(matrix, escale, inicio, fin, resultado))
+        hilo = threading.Thread(target=multiply_matrix_for_escale, args=(matrix, escale, inicio, fin, result))
         hilos.append(hilo)
         hilo.start()
 
     for hilo in hilos:
         hilo.join()
 
-    return resultado
+    return result
 
 def main():
     matrix = generate_matrix(*SIZE_MATRIX)
-
-    #print("matrix original:")
-    #for fila in matrix:
-    #    print(fila)
-
-    # Iniciar temporizador
     start_time = time.time()
-    #multiply_matrix_por_escale_secuencial(matrix, ESCALE)
-    multiply_matrix_por_escale_con_hilos(matrix, ESCALE, HILOS)
-    # Detener temporizador
+    #multiply_matrix_for_escale_secuencial(matrix, ESCALE)
+    multiply_matrix_for_escale_hilos(matrix, ESCALE, HILOS)
     end_time = time.time()
     elapsed_time = end_time - start_time
 
     print(f"\nmatrix resultante (tiempo de ejecución: {elapsed_time:.6f} segundos):")
-    #for fila in resultado:
-    #    print(fila)
 
 if __name__ == "__main__":
     main()
